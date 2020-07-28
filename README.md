9ed48181f57d1cf334daee38e5677bd041627071
a-nisa <68261493+a-nisa@users.noreply.github.com>
2020-07-28 13:19:20 +0530

    Added docs and changed color scheme

--- README.md
+++ README.md
@@ -1,30 +1,22 @@
 
 # PDF Viewer Web Application
-PDF Viewer is a simple web application that uses the Adobe DC View SDK to allow users to read and manipulate PDF files easily while maintaining quality.    
-
-## Run
-1. Change into the root directory and run `npm install` to install dependencies. 
-2. Run `node main.js` to start up very basic local server.
-3. Point to `localhost:3000` to interact with the application.    
-
-## Local File Access Feature
-Now, it is possible to access local files through the application by choosing via the *Choose a PDF File:* option.
+PDF Viewer is a simple web application that uses the Adobe PDF View SDK to allow users to read and manipulate PDF files easily while maintaining quality.    
 
 ## Structure
 The web app consists of a single `index.html` file and corresponding stylesheet `style.css` and scripting file `script.js` in the root directory. 
 
 The core functionality is contained in `script.js` which makes appropriate call to the **View SDK**, configures the `adobeDCView` object and supports *Embed View Mode switching*.
 
-## View SDK Credentials    
-̥As per discussions in the forum, the credentials used during development have been left intact so as to facilitate the reviewers during review.
+## PDF View SDK Credentials    
+̥The credentials used during development have been left intact so as to facilitate the reviewers during review.
 
 ## Google Analytics
 The analytics tracking code is placed under the `<head>` tag in `index.html`.
-> To setup your own google analytics, please swap the tracking code in `index.html` under the `<head>` tag with your own.
+> To setup your own google analytics, please swap the tracking code in `index.html` under the `<head>` tag with your own.   
+To know what is google analytics and how to set-up your own google analytics account, see [this article](https://analytics.google.com/analytics/academy/course/6)
 
-## Changing the PDF to render 
-See **Local File Access Feature**
+## Rendered PDF   
+As given in the description, the PDF rendered on the application is the one created by the `pdf_creator` tool. The ZIP needed to create the PDF is also included as `arms.zip`
 
-
-
-
+## Live App
+Visit [here](https://a-nisa.github.io/PDFViewer) to check out the application.
--- script.js
+++ script.js
@@ -1,49 +1,53 @@
-  
 const viewConfiguration = {
-    defaultViewMode: "FIT_PAGE",
-    embedMode:"FULL_WINDOW",
-    showPageControls:true,
-    dockPageControls:true,
-    showAnnotationsTools:true,
-    showLeftHandPanel:false,
-    showDownloadPDF:true,
-    showPrintPDF:true,
-    enableFormFilling:false
+	defaultViewMode: "FIT_PAGE",
+	embedMode: "FULL_WINDOW",
+	showPageControls: true,
+	dockPageControls: true,
+	showAnnotationsTools: true,
+	showLeftHandPanel: false,
+	showDownloadPDF: true,
+	showPrintPDF: true,
+	enableFormFilling: false
 };
 
-// the dafault PDF to render
+// the default PDF to render
 const fileDetails = {
-  fileURL:"https://a-nisa.github.io/PDFViewer/arms-gb-shaw.pdf",
-  fileName:"arms-gb-shaw.pdf"
+	fileURL: "https://a-nisa.github.io/PDFViewer/arms-gb-shaw.pdf",
+	fileName: "arms-gb-shaw.pdf"
 }
 
+/*
+ * Serves as the entry point to the application.
+ * Sets up event handlers and makes first call to embed sdk.
+ *
+ */
 let init = () => {
-  // setup event handlers for each button
-  document.getElementById("full-window-button").onclick = () => {
-    setViewMode('FULL_WINDOW');
-    // revert back to full window style for the body and div elements
-    document.getElementsByTagName('body').style = "margin: 0px";
-    document.getElementById('adobe-dc-pdf-view').style = "";
-  };
+	// setup event handlers for each button
+	document.getElementById("full-window-button").onclick = () => {
+		setViewMode('FULL_WINDOW');
+		// revert back to full window style for the body and div elements
+		document.getElementsByTagName('body').style = "margin: 0px";
+		document.getElementById('adobe-dc-pdf-view').style = "";
+	};
 
-  document.getElementById("inline-button").onclick = () => {
-    setViewMode('IN_LINE');
-    // revert back to inline style for the body and div elements
-    document.getElementsByTagName('body').style = "margin: 0px";
-    document.getElementById('adobe-dc-pdf-view').style = "";
-  };
+	document.getElementById("inline-button").onclick = () => {
+		setViewMode('IN_LINE');
+		// revert back to inline style for the body and div elements
+		document.getElementsByTagName('body').style = "margin: 0px";
+		document.getElementById('adobe-dc-pdf-view').style = "";
+	};
 
-  document.getElementById("sized-button").onclick = () => {
-    setViewMode('SIZED_CONTAINER');
-    // revert to sized container style
-    document.getElementsByTagName('body').style = "margin:100px 0 0 200px";
-    document.getElementById('adobe-dc-pdf-view').style = "height: 476px; width: 600px; border: 1px solid #dadada; display:inline-block;";
-  };
+	// revert to sized container style
+	document.getElementById("sized-button").onclick = () => {
+		setViewMode('SIZED_CONTAINER');
+		document.getElementsByTagName('body').style = "margin:100px 0 0 200px";
+		document.getElementById('adobe-dc-pdf-view').style = "height: 476px; width: 600px; border: 1px solid #dadada; display:inline-block;";
+	};
 
-  // call previewFile for the first time when sdk is ready
-  document.addEventListener("adobe_dc_view_sdk.ready" , () => {
-    previewFile(fileDetails);
-  });
+	// call previewFile for the first time when sdk is ready
+	document.addEventListener("adobe_dc_view_sdk.ready", () => {
+		previewFile(fileDetails);
+	});
 };
 
 
@@ -52,10 +56,10 @@ let init = () => {
  *  Also calls previewFile() to render PDF freshly
  */
 let setViewMode = (newEmbedMode) => {
-  if(newEmbedMode){
-    viewConfiguration.embedMode = newEmbedMode;
-  }
-  previewFile(fileDetails);
+	if (newEmbedMode) {
+		viewConfiguration.embedMode = newEmbedMode;
+	}
+	previewFile(fileDetails);
 };
 
 /*
@@ -63,64 +67,61 @@ let setViewMode = (newEmbedMode) => {
  * Also registers a callback with each Adobe event and sends that
  * event's data to Google Analytics. 
  */
-let previewFile = function(fileDetails){
-  
-  let adobeDCView = new AdobeDC.View({
-    clientId:"07d2235e9e4c4ac8bcbbd2454421bcca",
-    divId:"adobe-dc-pdf-view",
-  });
+let previewFile = function(fileDetails) {
 
-  adobeDCView.previewFile({
-    content:{
-      location:{
-        url:fileDetails.fileURL,
-      },
-    },
-    metaData:{
-      fileName:fileDetails.fileName,
-    }
-  }, viewConfiguration);
+	let adobeDCView = new AdobeDC.View({
+		clientId: "07d2235e9e4c4ac8bcbbd2454421bcca",
+		divId: "adobe-dc-pdf-view",
+	});
 
-  // set-up callback for all Adobe Events we are interested in
-  adobeDCView.registerCallback(
-    AdobeDC.View.Enum.CallbackType.EVENT_LISTENER,
-    (event) => {               
-      switch(event.type){
-        case "DOCUMENT_OPEN":           
-          ga("send", "event", {
-            eventCategory: "Adobe DC View SDK",
-            eventAction: "Document Open",
-            eventLabel:`event.data.fileName`
-          });
-          break;
-        case "PAGE_VIEW":
-           ga("send", "event", {
-                  eventCategory: "Adobe DC View SDK", 
-                  eventAction: "Page View",
-                  eventLabel:`${event.data.pageNumber} of ${event.data.fileName}` 
-              });
-          break;
-        case "DOCUMENT_DOWNLOAD":
-          ga("send", "event", {
-                eventCategory: "Adobe DC View SDK", 
-                eventAction: "Document Download",
-                eventLabel: event.data.fileName
-          });
-          break;
-        case "TEXT_COPY":
-          ga("send", "event",{
-                eventCategory: "Adobe DC View SDK", 
-                eventAction: "Text Copy",
-                eventLabel:`${event.data.copiedText} from ${event.data.fileName}`
-          });
-          break;
-        default:                
-      }
-    },
-    {
-        enablePDFAnalytics:true,                           
-        enableFilePreviewEvents:true
-    });  
+	adobeDCView.previewFile({
+		content: {
+			location: {
+				url: fileDetails.fileURL,
+			},
+		},
+		metaData: {
+			fileName: fileDetails.fileName,
+		}
+	}, viewConfiguration);
+
+	// set-up callback for all Adobe Events we are interested in
+	adobeDCView.registerCallback(
+		AdobeDC.View.Enum.CallbackType.EVENT_LISTENER,
+		(event) => {
+			switch (event.type) {
+				case "DOCUMENT_OPEN":
+					ga("send", "event", {
+						eventCategory: "Adobe DC Embed SDK",
+						eventAction: "Document Open",
+						eventLabel: `event.data.fileName`
+					});
+					break;
+				case "PAGE_VIEW":
+					ga("send", "event", {
+						eventCategory: "Adobe DC Embed SDK",
+						eventAction: "Page View",
+						eventLabel: `${event.data.pageNumber} of ${event.data.fileName}`
+					});
+					break;
+				case "DOCUMENT_DOWNLOAD":
+					ga("send", "event", {
+						eventCategory: "Adobe DC Embed SDK",
+						eventAction: "Document Download",
+						eventLabel: event.data.fileName
+					});
+					break;
+				case "TEXT_COPY":
+					ga("send", "event", {
+						eventCategory: "Adobe DC Embed SDK",
+						eventAction: "Text Copy",
+						eventLabel: `${event.data.copiedText} from ${event.data.fileName}`
+					});
+					break;
+				default:
+			}
+		}, {
+			enablePDFAnalytics: true,
+			enableFilePreviewEvents: true
+		});
 };
-
-
